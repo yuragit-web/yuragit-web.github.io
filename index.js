47f5ae0739ab36ce95ab2bae445b905f775a7e56
Yurii Shcherbiuk <28396907+yuragit-web@users.noreply.github.com>
2020-09-21 23:10:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -52,21 +52,21 @@ window.onload = function() {
             r.rg *= s;
             float g = .1, b = 1.;
             vec3 i = vec3(0.);
-            for (int l = 0; l < 36; l+=6) {
+            for (int l = 0; l < 20; l++) {
                 vec3 o = r + g * f * .5;
                 o = abs(vec3(1.) - mod(o, vec3(2.)));
                 float e, n = e = 0.;
-                for (int d = 0; d < 18; d++) {
+                for (int d = 0; d < 20; d++) {
                     o = abs(o) / dot(o, o) - .53;
                     n += abs(length(o) - e);
                     e = length(o);
                 }
-                if (l > 10) {
+                if (l > 6) {
                     b *= 1. - max(0., .3 - n * n * .001);
                 }
                 i += b + vec3(g, g * g, g * g * g * g) * n * n * n * .0015 * b;
                 b *= .73;
-                g += .2;
+                g += .1;
             }
             i = mix(vec3(length(i)), i, .85);
             gl_FragColor = vec4(i * .01, 1.);
