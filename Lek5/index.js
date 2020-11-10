6d9f94789056613fb58c2f669f3ebaf8935c4ea7
Yurii Shcherbiuk <28396907+yuragit-web@users.noreply.github.com>
2020-11-10 15:20:20 +0200

    Add files via upload

---
+++ Lek5/index.js
@@ -0,0 +1,3 @@
+console.log(document.head, document.children[0].children[0]);
+console.log(document.body.children[1], document.children[0].children[1].children[1]);
+console.log(document.body.children[1].children[1], document.children[0].children[1].children[1].children[1]);
