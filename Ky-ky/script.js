805a5ae3b86a9509949f84d6005d400e03f78f49
Yurii Shcherbiuk <28396907+yuragit-web@users.noreply.github.com>
2020-12-19 13:37:46 +0200

    Add files via upload

---
+++ Ky-ky/script.js
@@ -0,0 +1,84 @@
+class TextScramble {
+    constructor(el) {
+        this.el = el;
+        this.chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ0123456789!@#$%^&*()_+~`\'|}{[]\\:;?><,./-=';
+        this.update = this.update.bind(this);
+    }
+    setText(newText) {
+        const oldText = this.el.innerText;
+        const length = Math.max(oldText.length, newText.length);
+        const promise = new Promise(resolve => this.resolve = resolve);
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || '';
+            const to = newText[i] || '';
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
+            this.queue.push({
+                from,
+                to,
+                start,
+                end
+            });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+    }
+    update() {
+        let output = '';
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let {
+                from,
+                to,
+                start,
+                end,
+                char
+            } = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.25) {
+                    char = this.randomChar();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dud">${char}</span>`;
+            } else {
+                output += from;
+            }
+        }
+        this.el.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.resolve();
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
+
+const phrases = [
+    'Привет 😜',
+    'Как дела?',
+    'Как ты?',
+    'Что делаешь?'
+];
+
+const element = document.querySelector('.text');
+const textScramble = new TextScramble(element);
+
+let counter = 0;
+const next = () => {
+    textScramble.setText(phrases[counter]).then(() => {
+        setTimeout(next, 1500);
+    });
+    counter = (counter + 1) % phrases.length;
+};
+
+window.onload = next;
